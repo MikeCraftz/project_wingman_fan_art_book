e36110097304d6b3a175988affb84af234fd8e7d
YoyoyoMic <MikeCrazftz@users.noreply.github.com>
2025-12-27 19:50:43 +0900

    fixing url

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, abort
 
 app = Flask(__name__)
+app.config['FREEZER_RELATIVE_URLS'] = True
 
 # Data Structure: Chapters containing Pages
 CHAPTERS = [
