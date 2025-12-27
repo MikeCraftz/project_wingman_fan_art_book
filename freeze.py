d03d2d09ebb1edd08ac28c2560365a2b414ba8a2
YoyoyoMic <MikeCrazftz@users.noreply.github.com>
2025-12-27 15:43:47 +0900

    fixing freeze workflow

--- freeze.py
+++ freeze.py
@@ -1,12 +1,12 @@
 from flask_frozen import Freezer
-from app import app, PAGES
+from app import app, get_flat_pages
 
 freezer = Freezer(app)
 
 @freezer.register_generator
 def view():
-    for name in PAGES.keys():
-        yield {'name': name}
+    for page in get_flat_pages():
+        yield {'name': page['id']}
 
 if __name__ == '__main__':
     freezer.freeze()
